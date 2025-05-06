34d7cf81b227a9b99fb195013bd6f17db658843a
finn-rules <finn.rowles@gmail.com>
2025-05-06 14:52:13 -0500

    Create TextAdventure.java

---
+++ TextAdventure.java
@@ -0,0 +1,17 @@
+public class TextAdventure {
+    public static void main(String[] args) {
+        System.out.println("Dazed and confused, you find yourself in a dark hallway.");
+        Thread.sleep(2000);
+        System.out.println("You try to remember how you got here, but your mind is blanking. What's my name...? Where am I?");
+        Thread.sleep(2000);
+        System.out.println("Damn it, what's going on? Why don't I remember anything?");
+        Thread.sleep(2000);
+        System.out.println("You hear a faint sound coming from the end of the hallway.");
+        Thread.sleep(2000);
+        System.out.println("It's ominous, almost sounding like heavy footsteps. Heavy breathing echoes in the distance.");
+        Thread.sleep(2000);
+        System.out.println("You feel a chill down your spine.");
+        System.out.println("Every instinct in your memory-lacking body is telling you to run, or hide, or take some kind of action.");
+        Thread.sleep(2000);
+    }   
+}
