9dbd86aa00eb90933adc916175d9ea766d7ec839
finn-rules <finn.rowles@gmail.com>
2025-05-06 19:57:00 -0500

    Created room and its fields

--- Room.java
+++ Room.java
@@ -1,3 +1,8 @@
-public class Room {
-    
+public abstract class Room {
+    private String[] items;
+    private Room[] adjacentRooms; // Also a proxy for valid directions
+    private String lookAroundDescription;
+    private int waitStatus; // 0 : no wait.
+    private Obstacle[] obstacles;
+    private NPC npc;
 }
