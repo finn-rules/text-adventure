f7a6c0818d4cfd19186cae6007fcfeca5a275554
Emily <lackersh@grinnell.edu>
2025-05-06 19:51:22 -0500

    updated player

--- Player.java
+++ Player.java
@@ -1,3 +1,21 @@
 public class Player {
-    
+    int health;
+    String[] inventory;
+
+    public Player(int health, String[] inventory) {
+        this.health = health;
+        this.inventory = inventory;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public String[] getInventory() {
+        return inventory;
+    }
+    public String getInventoryIndex(int index) {
+        if (index < 0 || index >= inventory.length) {
+            throw new IndexOutOfBoundsException("you did not choose an index in inventory the array!"); // or throw an exception
+        }
+        return inventory[index];
+    }
 }
