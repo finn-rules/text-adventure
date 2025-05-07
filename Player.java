0ad209ff1b8f98224f5f08c9e825f1b141d24009
finn-rules <finn.rowles@gmail.com>
2025-05-06 20:25:29 -0500

    player, npc, obstacle (unfinished)

--- Player.java
+++ Player.java
@@ -1,17 +1,35 @@
 public class Player {
-    int health;
+    int maxHealth;
+    int curHealth;
+
+    boolean hidden;
     String[] inventory;
 
     public Player(int health, String[] inventory) {
-        this.health = health;
+        this.curHealth = health;
+        this.maxHealth = health;
+        this.hidden = false;
         this.inventory = inventory;
     }
-    public int getHealth() {
-        return health;
+    public int getCurHealth() {
+        return curHealth;
     }
+
+    public void takeDamage(int damage) {
+        this.curHealth -= damage;
+        if (this.curHealth <= 0) {
+            this.curHealth = 0;
+            System.out.println("You have been defeated!");
+            System.exit(0); // End the game
+        } else if (this.curHealth < this.maxHealth / 4) { 
+            System.out.println("That one hurt... your health is low! Stay strong, maybe consider fleeing!");
+        }
+    }
+
     public String[] getInventory() {
         return inventory;
     }
+
     public String getInventoryIndex(int index) {
         if (index < 0 || index >= inventory.length) {
             throw new IndexOutOfBoundsException("you did not choose an index in inventory the array!"); // or throw an exception
